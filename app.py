e8956cc81d4f1379316d535afc6c4421be6d6581
Yasser Alattas <y.alattas@gmail.com>
2021-09-18 03:52:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def query_example():
 def form_example():
     # handle the POST request
     if request.method == 'POST':
-        language = request.form.get('language')
-        framework = request.form.get('framework')
+        language = request.json['language']
+        framework = request.json['framework']
         return '''
                   <h1>The language value is: {}</h1>
                   <h1>The framework value is: {}</h1>'''.format(language, framework)
